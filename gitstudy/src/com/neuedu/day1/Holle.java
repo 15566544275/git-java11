cfa3c93260b17f6b370d2dc2ed60ca7f5a291433
15566544275 <2359324789@qq.com>
2020-08-19 18:15:10 +0800

    输入

--- gitstudy/src/com/neuedu/day1/Holle.java
+++ gitstudy/src/com/neuedu/day1/Holle.java
@@ -2,9 +2,7 @@ package com.neuedu.day1;
 
 public class Holle {
 	public static void main(String[] args) {
-		System.out.println("输出");
-		System.out.println("输出1");
-		System.out.println("输出33");
-		System.out.println("输出37");
+		System.out.println("输出");	
 	}
-}
+}                               
+              
