8beadcb62654d5e2029dfaca44f17ca1b922d855
15566544275 <69884527+15566544275@users.noreply.github.com>
2020-08-19 18:19:41 +0800

    Update Holle.java
    重来

--- gitstudy/src/com/neuedu/day1/Holle.java
+++ gitstudy/src/com/neuedu/day1/Holle.java
@@ -3,8 +3,8 @@ package com.neuedu.day1;
 public class Holle {
 	public static void main(String[] args) {
 		System.out.println("输出");
-		System.out.println("输出");		
-
+		System.out.println("输出");
+		System.out.println("输出1");
 	}
 }                               
               
