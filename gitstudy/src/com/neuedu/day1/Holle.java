8d1652f1edb372c858fefd92a824025347a6dff3
15566544275 <2359324789@qq.com>
2020-08-19 18:23:18 +0800

    最新输入

--- gitstudy/src/com/neuedu/day1/Holle.java
+++ gitstudy/src/com/neuedu/day1/Holle.java
@@ -4,7 +4,7 @@ public class Holle {
 	public static void main(String[] args) {
 		System.out.println("输出");
 		System.out.println("输出");
-		System.out.println("输出1");
+		System.out.println("输出2");
 	}
 }                               
               
