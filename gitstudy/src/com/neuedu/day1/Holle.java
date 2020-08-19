99751cf67188d64b3e131f93c230739bccb14a56
15566544275 <69884527+15566544275@users.noreply.github.com>
2020-08-19 18:15:52 +0800

    Update Holle.java
    原始

--- gitstudy/src/com/neuedu/day1/Holle.java
+++ gitstudy/src/com/neuedu/day1/Holle.java
@@ -2,10 +2,6 @@ package com.neuedu.day1;
 
 public class Holle {
 	public static void main(String[] args) {
-		System.out.println("输出");
-		System.out.println("输出1");
-		System.out.println("输出33");
-		System.out.println("输出37");
-		System.out.println("输出38");
+		System.out.println("输出");		
 	}
 }
